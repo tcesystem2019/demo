01511ccabb96422fef0a779b41ba631712a5123c
DESKTOP-NEMFN2M\caiyile <5745920203@qq.com>
2020-07-14 10:26:35 +0800

    API sign update

--- java/src/main/java/com/demo/bitcoin/test/Demo.java
+++ java/src/main/java/com/demo/bitcoin/test/Demo.java
@@ -30,18 +30,18 @@ public class Demo {
 ////        私钥：L4zteFp6S5wb7sQkNJyDcVwCUVQNRKq3FySPyGszWw1ppq9vsLHF
 
 //        // put请求
-        String putMethod = "/api/put";
-        String putUserAdds = "1DEPkC79Mr8QRa1w7NJxqA7RUP6VnHWHwB";                                                // dapp自身用户地址
-        String putDappAdds = "1FYxtYYKme9EjuZMsWVesJxrP75fAHo2jL";                                                // dapp地址,用于扣费
-        String putMetadata = "0101"; // 协议
-        String putData = "0101010101"; //上传内容转16进制数据
-        String putTimestamp = new Date().getTime() + "";  // 时间戳
-        StringBuilder putSign = new StringBuilder();
-        putSign.append(putMethod).append(putUserAdds).append(putDappAdds).append(putMetadata).append(putData).append(putTimestamp);
-        String hash = Sha256.getSHA256(putSign.toString());
-        String putSignature = signMsg(hash, "L4WAm7aJxmSDGhxgpWMjbm8NvgETae8S1d6L8P9gSpo6EF7nYhFm"); // 签名，把参数内容拼接，然后sha256， 再传入自己保存的私钥
-        JSONObject resultPutInfo = requestPut(putUserAdds, putDappAdds, putMetadata, putData, putTimestamp, putSignature);
-        System.out.println(resultPutInfo.toJSONString());
+//        String putMethod = "/api/put";
+//        String putUserAdds = "1DEPkC79Mr8QRa1w7NJxqA7RUP6VnHWHwB";                                                // dapp自身用户地址
+//        String putDappAdds = "1FYxtYYKme9EjuZMsWVesJxrP75fAHo2jL";                                                // dapp地址,用于扣费
+//        String putMetadata = "0101"; // 协议
+//        String putData = "0101010101"; //上传内容转16进制数据
+//        String putTimestamp = new Date().getTime() + "";  // 时间戳
+//        StringBuilder putSign = new StringBuilder();
+//        putSign.append(putMethod).append(putUserAdds).append(putDappAdds).append(putMetadata).append(putData).append(putTimestamp);
+//        String hash = Sha256.getSHA256(putSign.toString());
+//        String putSignature = signMsg(hash, "L4WAm7aJxmSDGhxgpWMjbm8NvgETae8S1d6L8P9gSpo6EF7nYhFm"); // 签名，把参数内容拼接，然后sha256， 再传入自己保存的私钥
+//        JSONObject resultPutInfo = requestPut(putUserAdds, putDappAdds, putMetadata, putData, putTimestamp, putSignature);
+//        System.out.println(resultPutInfo.toJSONString());
 //
 //         update请求
 //        String updateMethod = "/api/update";
@@ -49,7 +49,7 @@ public class Demo {
 //        String updateDappAdds = "1FYxtYYKme9EjuZMsWVesJxrP75fAHo2jL"; // 刚刚生成的地址
 //        String updateMetadata = "0101"; // 协议
 //        String updateData = "010101010101"; //上传内容转16进制数据
-//        String updateDirveId = "5f90e9b89e1f4b7399167089a713cb61b6dad7ac2398fd8cefbc2f5a61db5899";
+//        String updateDirveId = "89bf4e01465d56bb6fdf45f2ffb2d033631b5ba292bcf0f61d24c36387942913";
 //        String updateTimestamp = new Date().getTime() + "";  // 时间戳
 //        StringBuilder updateSign = new StringBuilder();
 //        updateSign.append(updateMethod).append(updateUserAdds).append(updateDappAdds).append(updateMetadata).append(updateData).append(updateDirveId).append(updateTimestamp);
@@ -93,20 +93,24 @@ public class Demo {
 //
 //
 //        // 查询指定driveId下的数据
-//        String driveIdMethod = "/api/get";
-//        String driveIdDappAdds = "1FYxtYYKme9EjuZMsWVesJxrP75fAHo2jL";
-//        String getdrive_id = "521f7b0d1cebf583c67238cf16b5a41eeec9ed64ef19e2cccd36b59790086c18";
-//        String driveIdTimestamp = new Date().getTime() + "";  // 时间戳
-//        StringBuilder driveIdSign = new StringBuilder();
-//        driveIdSign.append(driveIdMethod).append(driveIdDappAdds).append(getdrive_id).append(driveIdTimestamp);
-//        String driveIdSignature = signMsg(Sha256.getSHA256(driveIdSign.toString()), "KzGmcC2Fa48NHmad6SEst4Q1J9mHd95aFxzEuNrT94CEb6qrZAV6");
-//        JSONObject getDriveId = requestGetDriveid(driveIdDappAdds, getdrive_id, driveIdTimestamp, driveIdSignature);
-//        System.out.println(getDriveId);
+        String driveIdMethod = "/api/get";
+        String driveIdDappAdds = "1FYxtYYKme9EjuZMsWVesJxrP75fAHo2jL";
+        String getdrive_id = "89bf4e01465d56bb6fdf45f2ffb2d033631b5ba292bcf0f61d24c36387942913";
+        String driveIdTimestamp = new Date().getTime() + "";  // 时间戳
+        StringBuilder driveIdSign = new StringBuilder();
+        driveIdSign.append(driveIdMethod).append(driveIdDappAdds).append(getdrive_id).append(driveIdTimestamp);
+        String sha256 = Sha256.getSHA256(driveIdSign.toString());
+        String driveIdSignature = signMsg(sha256, "KzGmcC2Fa48NHmad6SEst4Q1J9mHd95aFxzEuNrT94CEb6qrZAV6");
+//        System.out.println(driveIdSign.toString());
+//        System.out.println(sha256);
+//        System.out.println(driveIdSignature);
+        JSONObject getDriveId = requestGetDriveid(driveIdDappAdds, getdrive_id, driveIdTimestamp, driveIdSignature);
+        System.out.println(getDriveId);
 
         // 查询指定updateId下的数据
 //        String updateIdMethod = "/api/get";
 //        String updateIdDappAdds = "1FYxtYYKme9EjuZMsWVesJxrP75fAHo2jL";
-//        String getupdate_id = "c1dbeae8073223980531cbca6715cafb5165025eafa78abc1ba45451ec8c94e0";
+//        String getupdate_id = "af639221bfb9890781ba3e4a870d296019ce054151db9f70177f5bbfd9627123";
 //        String updateIdTimestamp = new Date().getTime() + "";  // 时间戳
 //        StringBuilder updateIdSign = new StringBuilder();
 //        updateIdSign.append(updateIdMethod).append(updateIdDappAdds).append(getupdate_id).append(updateIdTimestamp);
@@ -125,6 +129,7 @@ public class Demo {
 //        JSONObject listAll = requestListAllDriveId(protocol, listAllDappAdds, listAllTimestamp, listAllSignature);
 //        System.out.println(listAll);
 
+//        charge("FNpBxrPyLckRZtkKccaw4mbSxHzwqzP3Ae","100000");
 
 
     }
@@ -221,6 +226,8 @@ public class Demo {
 
     }
 
+//    http://freedrive.cash:8880
+
     public static JSONObject requestGetTxHistory(String dapp_addr, Integer page, String timestamp, String signature) throws Exception {
 
         JSONObject param = new JSONObject();
@@ -253,6 +260,16 @@ public class Demo {
         return ecKey.signMessage(msg);
     }
 
+
+    public static JSONObject charge(String addr, String value) throws Exception {
+
+        Map param = new HashedMap();
+        param.put("addr", addr);
+        param.put("value", value);
+        JSONObject result = (JSONObject) JSONObject.parse(HttpUtil.doPost("http://freedrive.cash:8880/points/charge", param));
+        return result;
+
+    }
 
 
 
